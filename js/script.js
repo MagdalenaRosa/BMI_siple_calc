eb8a9797efdd30846b5634e8c5b539b82e4a5c71
Magdalena Rosa <magdalena.joanna.rosa@gmail.com>
2023-09-23 13:06:39 +0200

    initial commit -> BMI CALC-> SIPMLE

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function showValue_of_mase(){
+    var value=document.getElementById("mase");
+    var mase_var=document.getElementById("mase_result");
+    mase_var.innerHTML=value.value;
+}
+function showValue_of_heigh(){
+    var value=document.getElementById("height");
+    var variable=document.getElementById("heigt_result");
+    variable.innerHTML=value.value;
+}
